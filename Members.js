ce0e98380260a6aa34e53497d1d3a68c298fa3d5
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2023-10-31 12:41:41 +0530

    First commit

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Michael Johnson",
+    email: "michael.johnson@example.com",
+    status: "active",
+  },
+  {
+    id: 4,
+    name: "Emily Williams",
+    email: "emily.williams@example.com",
+    status: "inactive",
+  },
+];
+
+module.exports = members;
